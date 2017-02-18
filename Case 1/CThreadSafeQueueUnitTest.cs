9ececbcb7a1aaf5529213c2833035357e6b533de
dukovnukem <timofey>
2017-02-18 23:45:54 +0300

    Added tests

---
+++ Case 1/CThreadSafeQueueUnitTest.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ThreadSafeQueue;
+using System.Threading;
+
+namespace ThreadSafeQueueUnitTest
+{
+    [TestClass]
+    public class CThreadSafeQueueUnitTest
+    {
+        [TestMethod]
+        public void Push_Pop_SimpleTest()
+        {
+            var q = new CThreadSafeQueue<Int32>();
+            q.Push(123);
+            q.Push(0);
+            
+            Assert.AreEqual(q.Pop(), 123, "Wrong value");
+            Assert.AreEqual(q.Pop(), 0, "Wrong value");
+        }
+
+        [TestMethod]
+        [Timeout(3000)]//TestTimeout.Infinite)]
+        public void Pop_ShouldWaitForever()
+        {
+            var q = new CThreadSafeQueue<Int32>();
+            q.Pop();            
+        }
+
+        [TestMethod]
+        [Timeout(600*1000)] //10min
+        public void Push_Pop_MultithreadTest()
+        {
+            CThreadSafeQueue<String> q = new CThreadSafeQueue<String>();
+            List<Thread> threads = new List<Thread>();
+            Random rnd = new Random();
+
+            for (int i = 0; i < 100; i++)
+            {
+                threads.Add(new Thread(() =>
+                {
+                    for (int j = 0; j < 2000; j++)
+                    {
+                        Thread.Sleep(rnd.Next(0, 10));//this is a delay for test
+                        q.Push("TEST");
+                    }
+                }) { IsBackground = true });
+
+                threads.Add(new Thread(() =>
+                {
+                    for (int j = 0; j < 2000; j++)
+                    {
+                        Assert.AreEqual(q.Pop(), "TEST");
+                    }
+                }) { IsBackground = true });
+            }
+
+            foreach (var t in threads)
+                t.Start();
+
+            foreach (var t in threads)
+                t.Join();//in this test most important is that threads did not stuck
+        }
+    }
+}
---
+++ Case 2/ChoosePairsUnitTest.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ChoosePairs;
+
+namespace ChoosePairsUnitTest
+{
+    [TestClass]
+    public class CPairSelectorUnitTest
+    {
+        [TestMethod]
+        public void Select_CheckValues()
+        {
+            const Int32 X = 7;
+            ICollection<Int32> testArray = new[] { 1, -3, 9, 10, 11, 12 };
+            var pairs = CPairSelector.Select(testArray, X);
+            
+            Assert.AreEqual<Int32>(pairs.Count(), 1);
+            
+            foreach (var pair in pairs)
+            {
+                Assert.IsTrue((pair.Item1 == -3 && pair.Item2 == 10) || (pair.Item1 == 10 && pair.Item2 == -3));
+            }
+        }
+
+        [TestMethod]
+        public void Select_CheckCount()
+        {
+            const Int32 X = 98;
+            ICollection<Int32> testArrayY = new[] { -23, 17, 121, 6, 9, 95, 96, 7, 2, 5, 3, 108, -10, 578, 56, -480 };
+            var pairs = CPairSelector.Select(testArrayY, X);
+
+            Assert.AreEqual<Int32>(pairs.Count(), 5);
+        }
+    }
+}
